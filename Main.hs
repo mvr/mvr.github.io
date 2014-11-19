19a731e59bdd8cf09bc096e8184715f805993573
Mitchell Riley <mitchell.v.riley@gmail.com>
2014-11-19 18:25:44 +1000

    Initial site with no styling

---
+++ Main.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE OverloadedStrings  #-}
+
+module Main (main) where
+
+import Data.Monoid
+
+import Hakyll
+
+main :: IO ()
+main = hakyllWith config $ do
+  -- Compile posts
+  match "posts/*" $ do
+    route   $ setExtension ".html"
+    compile $ pandocCompiler
+      >>= loadAndApplyTemplate "templates/post.html" postCtx
+      >>= loadAndApplyTemplate "templates/default.html" defaultContext
+      >>= relativizeUrls
+
+  -- Post list
+  create ["posts.html"] $ do
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll "posts/*"
+      let ctx = constField "title" "Posts" <>
+                listField "posts" postCtx (return posts) <>
+                defaultContext
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/posts.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+  -- Compile templates
+  match "templates/*" $ compile templateCompiler
+
+postCtx :: Context String
+postCtx = dateField "date" "%B %e, %Y" <> defaultContext
+
+config :: Configuration
+config = defaultConfiguration
+    -- { deployCommand = "rsync --checksum -ave 'ssh -p 2222' \
+    --                   \_site/* jaspervdj@jaspervdj.be:jaspervdj.be"
+    -- }
---
+++ inprogress/jordancurve.md
@@ -0,0 +1,8 @@
+---
+title: The Jordan curve theorem
+published: March 17, 2014
+---
+
+Pick up a pen and draw some kind of squiggly loop so that you end where you start and don't cross your own path. The most obvious thing you could say about your picture is that the page has been split into two disjoint regions; the part inside your curve and the part outside. This assertion is known as the _Jordan curve theorem_. Not only is it something that needs to be proven, but a formal proof was only given 100 years ago!
+
+Alexander's horned sphere

52b21458f759b2f1922b75af91e917d988d73094
Mitchell Riley <mitchell.v.riley@gmail.com>
2014-12-12 11:52:41 +1000

    Add tags

--- Main.hs
+++ Main.hs
@@ -6,13 +6,16 @@ import Data.Monoid
 
 import Hakyll
 
+-- Much is stolen from https://github.com/jaspervdj/jaspervdj
 main :: IO ()
 main = hakyllWith config $ do
+  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+
   -- Compile posts
   match "posts/*" $ do
     route   $ setExtension ".html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html" postCtx
+      >>= loadAndApplyTemplate "templates/post.html" (postCtx tags)
       >>= loadAndApplyTemplate "templates/default.html" defaultContext
       >>= relativizeUrls
 
@@ -22,7 +25,35 @@ main = hakyllWith config $ do
     compile $ do
       posts <- recentFirst =<< loadAll "posts/*"
       let ctx = constField "title" "Posts" <>
-                listField "posts" postCtx (return posts) <>
+                listField "posts" (postCtx tags) (return posts) <>
+                defaultContext
+      makeItem ""
+        >>= loadAndApplyTemplate "templates/posts.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+  -- Index
+  match "index.html" $ do
+    route idRoute
+    compile $ do
+      posts <- fmap (take 3) . recentFirst =<< loadAll "posts/*"
+      let ctx = listField "posts" (postCtx tags) (return posts)
+                <> field "Tags" (\_ -> renderTagList tags)
+                <> defaultContext
+      getResourceBody
+        >>= applyAsTemplate ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+  -- Tags
+  tagsRules tags $ \tag pattern -> do
+    let title = "Posts tagged " ++ tag
+
+    route idRoute
+    compile $ do
+      posts <- recentFirst =<< loadAll pattern
+      let ctx = constField "title" title <>
+                listField "posts" (postCtx tags) (return posts) <>
                 defaultContext
       makeItem ""
         >>= loadAndApplyTemplate "templates/posts.html" ctx
@@ -37,8 +68,10 @@ main = hakyllWith config $ do
     route $ gsubRoute "public/" (const "")
     compile copyFileCompiler
 
-postCtx :: Context String
-postCtx = dateField "date" "%B %e, %Y" <> defaultContext
+postCtx :: Tags -> Context String
+postCtx tags = dateField "date" "%B %e, %Y"
+               <> tagsField "tags" tags
+               <> defaultContext
 
 config :: Configuration
 config = defaultConfiguration
